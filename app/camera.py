ca029c663b37e9066fd4c6ff09a2847436fc7214
muhammedarsal1 <aiarsal2006@gmail.com>
2025-03-14 14:57:10 +0530

    Fixed camera access issue

--- app/camera.py
+++ app/camera.py
@@ -1,17 +1,15 @@
 import cv2
-import streamlit as st
 
 def get_frame():
-    cap = cv2.VideoCapture(0)  # Use 0 for the default webcam
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # Use CAP_DSHOW for Windows
+
     if not cap.isOpened():
-        st.error("Could not open webcam. Please allow camera access.")
         return None
 
     ret, frame = cap.read()
     cap.release()
 
     if not ret:
-        st.error("Failed to capture image from camera.")
         return None
 
     return frame
